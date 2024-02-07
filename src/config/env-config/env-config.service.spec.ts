f7bb354f422fc15e53ffaf5b25628878a3a343ef
MarkacRobi <robi.markac@gmail.com>
2024-02-07 09:34:36 +0100

    fix: tests

--- src/config/env-config/env-config.service.spec.ts
+++ src/config/env-config/env-config.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { EnvConfigService } from "./env-config.service";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 describe("EnvConfigService", () => {
     let service: EnvConfigService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [EnvConfigService],
+            imports: [
+                ConfigModule.forRoot({
+                    envFilePath: `.env.${process.env.NODE_ENV}`,
+                }),
+            ],
+            providers: [EnvConfigService, ConfigService],
+            exports: [EnvConfigService],
         }).compile();
 
         service = module.get<EnvConfigService>(EnvConfigService);
